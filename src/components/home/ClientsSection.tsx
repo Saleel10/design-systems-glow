96c42d5d9d60caa2ce8a99444a5b0fc96e105739
Saleel10 <salisaleel2004@gmail.com>
2026-05-12 20:35:53 +0530

    fr

--- src/components/home/ClientsSection.tsx
+++ src/components/home/ClientsSection.tsx
@@ -66,10 +66,10 @@ const ClientsSection = () => {
 
         {/* Logo Slider */}
         <div className="relative">
-          {/* Left Blur */}
+          {/* Left Gradient */}
           <div className="pointer-events-none absolute inset-y-0 left-0 w-32 bg-gradient-to-r from-background to-transparent z-10" />
 
-          {/* Right Blur */}
+          {/* Right Gradient */}
           <div className="pointer-events-none absolute inset-y-0 right-0 w-32 bg-gradient-to-l from-background to-transparent z-10" />
 
           <motion.div
